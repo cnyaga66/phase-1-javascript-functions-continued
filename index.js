e3b601b9b65f0c8e26f5bc93259c440c3c9d2227
cnyaga66 <cnyga66@gmail.com>
2024-03-24 13:01:43 +0300

    my first fubctions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function myFunction (saturdayfun) {
+    console.log("Rollerskate!");
+}
+function myFunction (Monday work) {
+    console.log ("Go to the office!");
+}
+
+function myFunction (WrapAdjective) {
+    WrapAdjective="%"
+    return WrapAdjective(%)("A cooperative graphic designer"); =>  "A %-cooperative graphic designer%!"
+}
+
+
+
